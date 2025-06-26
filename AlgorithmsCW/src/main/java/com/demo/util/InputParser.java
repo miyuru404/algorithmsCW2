0b5d916fbe3442c024b83468d2a8ae3015e977e0
Miyuru Bashitha <miyurubashitha100@gmail.com>
2025-06-26 14:52:44 +0530

    add: add two main function for InputParser class

--- AlgorithmsCW/src/main/java/com/demo/util/InputParser.java
+++ AlgorithmsCW/src/main/java/com/demo/util/InputParser.java
@@ -2,4 +2,20 @@ package com.demo.util;
 
 public class InputParser {
 
+    /*
+        This is the method "parser" that responsible for reading values from the file.
+        This will read all the values in the file and validate the input using the helper function call "validateInput()".
+        This method will use path of the input file as a string And return the original tree data if validated.
+    */
+    public static int[] parser(String filePath){
+        return null;
+    }
+
+    /*
+        helpar funtion thate validate the data
+     */
+    private static void validateInput(){
+
+    }
+
 }
